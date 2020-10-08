a91087639cc0a639e580866cc5a278942ccc9116
lodi <lodi.dev@outlook.com>
2020-10-07 23:47:32 -0500

    before to add

--- ProyectoDjango/blog/admin.py
+++ ProyectoDjango/blog/admin.py
@@ -6,10 +6,15 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
     # lleva una coma para que se interprete como una tupla
     readonly_fields = ('created_at',)
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
 
 
 class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at', 'user')
+    # el doble __ es para acceder a una propiedade de un modelo
+    list_filter = ('title', 'public', 'user')
+    list_display = ('title', 'public', 'user')
 
     # modificar algo cuando se guarde
     # los parametros son necesarios
--- ProyectoDjango/pages/admin.py
+++ ProyectoDjango/pages/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from .models import *
 
+
+# Configuración extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('create_at', 'updated_at')
+    #agregar buscador
+    search_fields = ('title', 'content')
+    #filtro visible
+    list_filter = ('visible',)
+    #sacar columnas en la lista
+    list_display = ('title', 'visible' )
+    #ordenar por defecto
+    ordering = ('-create_at',)
+
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 
-#configutacion del panel
+# configutacion del panel
 title = "Proyecto Django"
 subtitle = "Panel de gestión"
 
